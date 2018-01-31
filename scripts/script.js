a8ce5e1703890b5b354f5ca00e18c976ae66ea97
Brad Swan <bradj.swan@gmail.com>
2018-01-31 12:47:21 +1000

    fixed css positioning, created javascript functions

--- scripts/script.js
+++ scripts/script.js
@@ -3,24 +3,28 @@ var linebreak = document.createElement("br");
 
 let addDiv = document.createDocumentFragment();
 
-for (var i = 0; i < 16; i++) {		
-	for (var j = 0; j < 16; j++) {
-	var div = document.createElement("div");
-	div.className = "pixel";
-	addDiv.appendChild(div);
+function createGrid() {
+	for (var i = 0; i < 16; i++) {		
+		for (var j = 0; j < 16; j++) {
+		var div = document.createElement("div");
+		div.className = "pixel";
+		addDiv.appendChild(div);
+		}
+		container.appendChild(addDiv);
+		var linebreak = document.createElement("br");
+		container.appendChild(linebreak);
 	}
-	container.appendChild(addDiv);
-	var linebreak = document.createElement("br");
-	container.appendChild(linebreak);
-}
+}	
 
-var pixels = document.getElementsByClassName("pixel");
-for (i = 0; i < pixels.length; i++) {
-	pixels[i].addEventListener("mouseover", function() {
-		this.style.backgroundColor = "black";
-		});
+function drawColor() {
+	let color = prompt("Enter pen color:");
+	var pixels = document.getElementsByClassName("pixel");
+	for (i = 0; i < pixels.length; i++) {
+		pixels[i].addEventListener("mouseover", function() {
+			this.style.backgroundColor = color;
+			});
+	}
 }
-
 
 
 
